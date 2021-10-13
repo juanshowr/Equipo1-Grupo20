218f600c005dd35486cf67920892a8db96a6db38
Oscar Amaya <91256282+66mayas@users.noreply.github.com>
2021-10-13 16:31:57 -0500

    Centrado título en detalle producto

--- proyecto-web-crud/frontend/src/components/detalleProductoComponent.vue
+++ proyecto-web-crud/frontend/src/components/detalleProductoComponent.vue
@@ -1,106 +1,107 @@
 <template>
     <div class="container ">
-            <main>
-              <div >
-                <h2 >{{producto.nombre}}</h2>
-                <p >{{producto.precio_venta}}</p>
-              </div>
-          
-              <div class="row g-3">
-                    <div class="col-md-4 order-md-last">
-                        <p id="grey_texts">{{producto.detalle}}</p>
-                    </div>
-                    <div class="col-md-3 col-lg-3">
-                        <a href="#" class="list-group-item list-group-item-action active" aria-current="true">
-                            The current link item
-                        </a>
-                        <a href="#" class="list-group-item list-group-item-action">Insumos avícolas</a>
-                        <a href="#" class="list-group-item list-group-item-action">Medicina Veterinaria</a>
-                        <a href="#" class="list-group-item list-group-item-action">Concentrados</a>
-                        <a class="list-group-item list-group-item-action disabled">Atención Veterinaria</a>
-                    </div>
-                    <div class="col-md-5 col-lg-3">
-                        <img v-bind:src="producto.imagen" alt="" width="230" height="340">
-                    </div>
-              </div>
-              <br>
-              <br>
-              <br>
-              <div class="row mx-auto my-auto justify-content-center py-5">
-                <div id="recipeCarousel" class="carousel slide" data-bs-ride="carousel">
-                    <div class="carousel-inner" role="listbox">
-                        <div class="carousel-item active">
-                            <div class="col-md-3">
-                              <center>
-                                <a  href="https://www.google.com/">
-                                  <img src="../assets/logoB.jpg" class="img-fluid" width="150" height="50">
-                                </a>
-                              </center>
- 
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <div class="col-md-3">
-                                <div class="card">
-                                    <div class="card-img">
-                                        <img src="//via.placeholder.com/500x400/e66?text=2" class="img-fluid">
-                                    </div>
-                                    <div class="card-img-overlay">Slide 2</div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <div class="col-md-3">
-                                <div class="card">
-                                    <div class="card-img">
-                                        <img src="//via.placeholder.com/500x400/7d2?text=3" class="img-fluid">
-                                    </div>
-                                    <div class="card-img-overlay">Slide 3</div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <div class="col-md-3">
-                                <div class="card">
-                                    <div class="card-img">
-                                        <img src="//via.placeholder.com/500x400?text=4" class="img-fluid">
-                                    </div>
-                                    <div class="card-img-overlay">Slide 4</div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <div class="col-md-3">
-                                <div class="card">
-                                    <div class="card-img">
-                                        <img src="//via.placeholder.com/500x400/aba?text=5" class="img-fluid">
-                                    </div>
-                                    <div class="card-img-overlay">Slide 5</div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <div class="col-md-3">
-                                <div class="card">
-                                    <div class="card-img">
-                                        <img src="//via.placeholder.com/500x400/fc0?text=6" class="img-fluid">
-                                    </div>
-                                    <div class="card-img-overlay">Slide 6</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <a class="carousel-control-prev bg-transparent w-aut" href="#recipeCarousel" role="button" data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    </a>
-                    <a class="carousel-control-next bg-transparent w-aut" href="#recipeCarousel" role="button" data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    </a>
+        <main>
+            <div class="row g-2">
+                <div class="col-md-9">
+                    <h2 class="cabecera">{{producto.nombre}}</h2>
+                    <p id="precio" class="cabecera">$ {{producto.precio_venta}}</p>
                 </div>
             </div>
-            </main>
+          <div class="row g-3">
+                <div class="col-md-4 order-md-last">
+                    <p id="grey_texts">{{producto.detalle}}</p>
+                </div>
+                <div class="col-md-3 col-lg-3">
+                    <a href="#" class="list-group-item list-group-item-action active" aria-current="true">
+                        Categorías
+                    </a>
+                    <a href="#" class="list-group-item list-group-item-action">Insumos avícolas</a>
+                    <a href="#" class="list-group-item list-group-item-action">Medicina Veterinaria</a>
+                    <a href="#" class="list-group-item list-group-item-action">Concentrados</a>
+                    <a class="list-group-item list-group-item-action disabled">Atención Veterinaria</a>
+                </div>
+                <div class="col-md-5 col-lg-3">
+                    <img v-bind:src="producto.imagen" alt="" width="230" height="340">
+                </div>
+          </div>
+          <br>
+          <br>
+          <br>
+          <div class="row mx-auto my-auto justify-content-center py-5">
+            <div id="recipeCarousel" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner" role="listbox">
+                    <div class="carousel-item active">
+                        <div class="col-md-3">
+                          <center>
+                            <a  href="https://www.google.com/">
+                              <img src="../assets/logoB.jpg" class="img-fluid" width="150" height="50">
+                            </a>
+                          </center>
+
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="col-md-3">
+                            <div class="card">
+                                <div class="card-img">
+                                    <img src="//via.placeholder.com/500x400/e66?text=2" class="img-fluid">
+                                </div>
+                                <div class="card-img-overlay">Slide 2</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="col-md-3">
+                            <div class="card">
+                                <div class="card-img">
+                                    <img src="//via.placeholder.com/500x400/7d2?text=3" class="img-fluid">
+                                </div>
+                                <div class="card-img-overlay">Slide 3</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="col-md-3">
+                            <div class="card">
+                                <div class="card-img">
+                                    <img src="//via.placeholder.com/500x400?text=4" class="img-fluid">
+                                </div>
+                                <div class="card-img-overlay">Slide 4</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="col-md-3">
+                            <div class="card">
+                                <div class="card-img">
+                                    <img src="//via.placeholder.com/500x400/aba?text=5" class="img-fluid">
+                                </div>
+                                <div class="card-img-overlay">Slide 5</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="col-md-3">
+                            <div class="card">
+                                <div class="card-img">
+                                    <img src="//via.placeholder.com/500x400/fc0?text=6" class="img-fluid">
+                                </div>
+                                <div class="card-img-overlay">Slide 6</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <a class="carousel-control-prev bg-transparent w-aut" href="#recipeCarousel" role="button" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                </a>
+                <a class="carousel-control-next bg-transparent w-aut" href="#recipeCarousel" role="button" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                </a>
+            </div>
         </div>
-    
+        </main>
+    </div>
+
 </template>
 
 
@@ -122,3 +123,15 @@ export default {
   }
 };
 </script>
+
+<style>
+    .cabecera {
+        display: block;
+        text-align: center;
+        font-weight: bold;
+    }
+
+    #precio {
+        border: 1px solid silver;
+    }
+</style>
