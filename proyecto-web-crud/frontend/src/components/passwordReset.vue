3b6a365627e105495eea19f8cf9cbf1f3b5ed5fb
Oscar Amaya <91256282+66mayas@users.noreply.github.com>
2021-10-13 06:51:06 -0500

    Agregado esquema componente passwordReset
    Se crea el componente donde va a ir el formulario de restablecimiento de contraseña

---
+++ proyecto-web-crud/frontend/src/components/passwordReset.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <h4>Aquí va el formulario para restablecer la contraseña</h4>
+      <ul>
+        <li>Tendrá un campo para que el usuario escriba su correo electrónico</li>
+        <li>Otro campo para que escriba el código que el sistema le envía al correo.</li>
+        <li>Tendrá un botón Continuar desactivado inicialmente.</li>
+        <li>Después de que escriba un código válido se activará el botón Continuar</li>
+      </ul>
+  </div>
+</template>
+
+<script>export default {}</script>
+
+<style></style>
