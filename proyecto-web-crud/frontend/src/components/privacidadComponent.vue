501aed0cb786a4d87bbb42a385e5724e73962d48
Oscar Amaya <91256282+66mayas@users.noreply.github.com>
2021-10-14 13:32:13 -0500

    Agregado componente de políticas de privacidad

---
+++ proyecto-web-crud/frontend/src/components/privacidadComponent.vue
@@ -0,0 +1,40 @@
+
+<template>
+  <v-container>
+    <section class="py-5">
+      <div class="container contenedor px-4 px-lg-5 mt-1">
+        <div class="p-4">
+          <h1>Política de privacidad</h1>
+        </div>
+        <div class="">
+          <p>En cumplimiento de la Ley Estatutaria 1581 de 2012 de Protección de Datos (LEPD) y las normas que la reglamenten, el presente Aviso de Privacidad tiene como objeto obtener la autorización expresa e informada del Titular para el tratamiento y la transferencia de sus datos a terceras entidades.</p>
+          <p>Las condiciones del tratamiento son las siguientes:</p>
+          <ul>
+            <li>
+              <p>AGROPECUARIA EL GALPÓN, identificado con el NIT 555 888 222-7, será el responsable del tratamiento de sus datos personales.</p>
+            </li>
+            <li>
+              <p>Con el objeto de recibir una atención integral como cliente, los datos personales recibidos serán tratados con las siguientes finalidades: campañas de fidelización de clientes, gestión de clientes, gestión administrativa, marketing, prospección comercial y el envío de comunicaciones comerciales sobre nuestros productos y/o servicios.</p>
+            </li>
+            <li>
+              <p>La política de tratamiento de datos del Titular, así como los cambios sustanciales que se produzcan en esta, se podrán consultar en el siguiente correo electrónico: protecciondatos@elgalpon.com</p>
+            </li>
+            <li>
+              <p>El Titular puede ejercer los derechos de acceso, corrección, supresión, renovación o reclamo por infracción de sus datos con un escrito dirigido a AGROPECUARIA EL GALPÓN a la dirección de correo electrónico protecciondatos@elgalpon.com, indicando en el asunto, el derecho que desea ejercer, o mediante correo postal enviado a la Carrera 6 # 6 – 43 en Pereira.</p>
+            </li>
+          </ul>
+        </div>                
+      </div>
+    </section>
+  </v-container>
+</template>
+
+<script>
+  export default {};
+</script>
+
+<style>
+  .contenedor{
+    background-color: rgb(167, 236, 213);
+  }
+</style>
