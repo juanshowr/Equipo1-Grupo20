964060400cbaba1aafa53839d35b9867b67271f3
FelipeRLeon <feliiperestrepoleon@gmail.com>
2021-10-14 17:47:51 -0500

    Modificacion pagina terminos y condiciones

--- proyecto-web-crud/frontend/src/components/terminosComponent.vue
+++ proyecto-web-crud/frontend/src/components/terminosComponent.vue
@@ -2,12 +2,29 @@
 <template>
     <v-container>
     <section class="py-5">
-            <div class="container contenedor px-4 px-lg-5 mt-1"><!--trabajr desde aca-->
+            <div class="container contenedor px-4 px-lg-5 pb-4 mt-1"><!--trabajr desde aca-->
                 <div class="p-4">
                     <h1>Terminos y condiciones</h1>
                 </div>
                 <div class="">
-                    <p>Exercitation enim et voluptate dolor dolore. Commodo et ea aliquip sit fugiat. Sunt sit velit laborum culpa eu laboris laborum qui nisi laborum duis. Mollit enim magna laborum labore deserunt dolor. Reprehenderit laboris in mollit est occaecat irure irure. Ullamco labore nisi exercitation aliqua nisi consectetur excepteur occaecat officia laboris. Proident magna id in ullamco cillum qui cillum irure fugiat.</p>
+                  <h5>1. Propiedad de este Sitio</h5>
+                  <p>Estos Términos y Condiciones de Uso aplican a este Sitio Web disponible en disponible en http://www.elgalpon.com.co/</p>
+
+                  <h5>2. Aceptación de estos Términos y Condiciones de Uso</h5>
+                  <p>El usuario debe leer siempre estos Términos y Condiciones de Uso antes de utilizar este Sitio Web‎, el uso que haga del Sitio Web supondrá que ha leído y acepta los presentes Términos y Condiciones de Uso.<br> <br>
+                  Si no está de acuerdo con estos Términos y Condiciones de Uso no utilice este Sitio Web.</p>
+
+                  <h5>3. Modificaciones</h5>
+                  <p>El galpon se reserva el derecho de modificar en forma unilateral estos Términos y Condiciones de Uso, en un todo o en parte, en cualquier momento y sin previo aviso, haciendo público en el Sitio Web los términos modificados. Podrán ser modificados también de la misma manera el diseño, la presentación o configuración, los requisitos de registro o utilización del Sitio Web, sin que ello genere derecho a reclamo o indemnización alguna a favor del usuario.<br><br>Es responsabilidad del usuario revisar periódicamente los Términos y Condiciones de Uso. El uso continuo que haga del Sitio Web luego de haberse realizado cambios en estos Términos y Condiciones de Uso supondrá que acepta esos cambios.</p>
+
+                  <h5>4. Contenido</h5>
+                  <p>Todos los textos, gráficos, diseños, interfaces de usuario, fotografías, logos, música, emblemas comerciales y código fuente (en adelante el Contenido) del Sitio Web son propiedad de Agrovelca, El Contenido del Sitio Web se encuentra protegido por derechos de autor.<br><br>El Contenido de este Sitio Web no puede ser copiado, reproducido, posteado, transmitido, republicado, trasladado o distribuido de ninguna manera a otro computador, servidor, o cualquier otro medio sin autorización por escrito de El Galpon.Las etiquetas, opiniones y calificaciones expresadas por los usuarios sobre los productos en el Sitio Web no representan una asesoría ni la opinión de El Galpon.<br><br>El Galpon no revisa cada una de las etiquetas, calificaciones ni opiniones expresadas por los usuarios. Sin embargo se reserva el derecho de aceptarlas ó eliminarlas parcial ó totalmente, en cualquier momento, a su discreción, sin previo aviso y sin dar explicaciones al usuario que las emite.El Galpon se reserva el derecho de corregir en cualquier momento y en forma unilateral cualquier error u omisión en cualquier Contenido publicado en el Sitio Web.</p>
+
+                  <h5>6. Terminación de Uso</h5>
+                  <p>El Galpon se reserva el derecho unilateral de restringir el acceso en caso de detectar cualquier uso no permitido del Sitio Web y tomar las acciones legales correspondientes.</p>
+
+                  <h5>7. Privacidad</h5>
+                  <p>El Gaplon cuenta con una Política de Privacidad la cual hace parte integral de estos Términos y Condiciones de Uso.</p>
                     
                 </div>
                         
