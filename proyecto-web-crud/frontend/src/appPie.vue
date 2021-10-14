afe083a90e37acfa67fbc2e8c4195325dd0a828c
Oscar Amaya <91256282+66mayas@users.noreply.github.com>
2021-10-14 13:38:57 -0500

    Agregado link de privacidad al footer

--- proyecto-web-crud/frontend/src/appPie.vue
+++ proyecto-web-crud/frontend/src/appPie.vue
@@ -1,10 +1,15 @@
 <template>
     <!-- Footer-->
         <footer class="py-5 bg-dark">
-            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; El Galpon 2021</p></div>
+            <div class="container">
+                <p class="m-0 text-center text-white">Copyright &copy; El Galpon 2021</p>
+            </div>
 
             <div class="col-auto text-center">
-            <router-link class="m-0 text-center text-white" to="/terminos">Terminos y Condiciones</router-link>
+                <router-link class="m-0 text-center text-white" to="/terminos">Terminos y Condiciones</router-link>
+            </div>
+            <div class="col-auto text-center">
+                <router-link class="m-0 text-center text-white" to="/privacidad">Política de privacidad</router-link>
             </div>
         </footer>
 </template>
